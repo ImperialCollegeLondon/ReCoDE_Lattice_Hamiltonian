5042d7434444a24841e4b0591b53296cfb40352b
lucy-hart <91886785+lucy-hart@users.noreply.github.com>
2025-02-03 13:02:37 +0000

    Update Lattice.py

--- lattice_hamiltonian/Lattice.py
+++ lattice_hamiltonian/Lattice.py
@@ -17,8 +17,8 @@ import scipy.constants as sp
 from scipy import linalg
 from scipy.sparse import csr_array
 
-from lattice_hamiltonian.Recombination import Recombination 
-import lattice_hamiltonian.Redfield import Redfield 
+import lattice_hamiltonian.Recombination 
+import lattice_hamiltonian.Redfield 
 
 
 @dataclass
